0efee0dcd63be398512f6953af31cad9dab4fbc5
Armando Martins <armando.amds97@gmail.com>
2025-04-01 09:17:24 -0300

    atualização do exercicio anterior, adicionei aumento

--- Exercicio01.py
+++ Exercicio01.py
@@ -4,6 +4,11 @@
 nome = input("Insira o seu nome: ")
 idade = int(input("Insira a sua idade: "))
 salario = float(input("Informe o seu salario: "))
+aumentoSalario = float(input("Infome a porcentagem do salario: "))
+
+#processamento
+aumento = (salario*aumentoSalario)/100
+novoSalario = salario+aumento
 
 #saida de dados
-print(f"Olá {nome}, você tem {idade} anos de vida e possui um salário de R$ {salario} mensais.")
+print(f"Olá {nome}, você tem {idade} anos de vida. O seu salário era de R${salario:.2f} mensais.\nObteve um aumento de R${aumento:.2f}\nSeu novo salario é de R${novoSalario:.2f} ")
