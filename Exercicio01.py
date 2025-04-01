097d6868594de171ff0ddace6d0c4d9ca6947e75
Armando Martins <armando.amds97@gmail.com>
2025-04-01 08:53:42 -0300

    primeiro exercicio do dia

---
+++ Exercicio01.py
@@ -0,0 +1,9 @@
+#faça um programa para ler o nome de uma pessoa, a sua idade e o salario e mostre essas informações
+
+#entrada de dados
+nome = input("Insira o seu nome: ")
+idade = int(input("Insira a sua idade: "))
+salario = float(input("Informe o seu salario: "))
+
+#saida de dados
+print(f"Olá {nome}, você tem {idade} anos de vida e possui um salário de R$ {salario} mensais.")
