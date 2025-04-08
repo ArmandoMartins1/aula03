942eaa6f5abc23de36f81493e4f3c62f8f2cf8db
Armando Martins <armando.amds97@gmail.com>
2025-04-08 09:33:01 -0300

    adicionando novo exercicio, usando numeros de 1 a 12, ele exibe o mês referente ao numero, usando (if/elif/else)

---
+++ Exercicio9.py
@@ -0,0 +1,33 @@
+"""receba, do usuario, um numero de 1 a 12 e mostre o nome do mês correspondente.
+caso o mês não exista, mostrar: "Valor inválido".
+obs: usando if/elif/else"""
+#entrada de dados
+num = int(input("Digite um numero entre 1 e 12: "))
+
+#processamento
+if num == 1:
+    print("Janeiro")
+elif num == 2:
+    print("Fevereiro")
+elif num == 3:
+    print("Março")
+elif num == 4:
+    print("Abril")
+elif num == 5:
+    print("Maio")
+elif num == 6:
+    print("Junho")
+elif num == 7:
+    print("Julho")
+elif num == 8:
+    print("Agosto")
+elif num == 9:
+    print("Setembro")
+elif num == 10:
+    print("Outubro")
+elif num == 11:
+    print("Novembro")
+elif num == 12:
+    print("Dezembro")
+else:
+    print("Valor Inválido!")
